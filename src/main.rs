dad9c491649154e42e0501a8591f37cfa6414850
Peter Keller <pmkellerza@gmail.com>
2020-12-17 06:19:05 +0200

    Basic Structure of App
    Added Structs and Implements
    
    Added functions to get user inputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
+use std::collections::HashMap;
+use std::io::{stdin, self, Write};
+
+#[derive(Eq, PartialEq, Debug, Default)]
+struct Contact {
+    first_name: String,
+    surname: String,
+    date_of_birth: String,
+    address: String,
+    tel: String,
+    email: String,
+    contact_book: Vec<HashMap<String,String>>,
+}
+
+#[allow(dead_code)]
+impl Contact {
+    fn new (first_name: String, surname: String, date_of_birth: String, address: String, tel: String, email: String) -> Self {
+        Self {
+            first_name: first_name,
+            surname: surname,
+            date_of_birth: date_of_birth,
+            address: address,
+            tel: tel,
+            email: email,
+            contact_book: vec![]
+        }
+    }
+
+
+    //Function used to push struct data into struct vector of hashmaps contact_book
+    fn data_push(&mut self) {
+        let mut contact_hash =  HashMap::new();
+
+        contact_hash.insert("Name".to_string(), self.first_name.trim_end().to_string());
+        contact_hash.insert("Surname".to_string(), self.surname.trim_end().to_string());
+        contact_hash.insert("DateofBirth".to_string(),self.date_of_birth.trim_end().to_string());
+        contact_hash.insert("Address".to_string(), self.address.trim_end().to_string());
+        contact_hash.insert("Tel".to_string(), self.tel.trim_end().to_string());
+        contact_hash.insert("Email".to_string(), self.email.trim_end().to_string());
+
+        self.contact_book.push(contact_hash);
+    }
+
+    //Function to print contact book to screen
+    fn print_data(&self) {
+        //TODO make a loop to iterate through the vector contact_book and display the hashmaps contents in a nice formatted way
+        println!("{:?}", self.contact_book);
+    }
+}
+
+// Function for getting user input and returns the input
+fn input_capture(text: &str) -> String {
+    let mut input = String::new();
+
+    println!("{}: ", text);
+    io::stdout().flush().unwrap();
+
+    stdin().read_line(&mut input)
+        .expect("Did not enter a correct string");
+
+    return input;
+}
+
+//Functions calls input_capture and stores into struct
+fn  user_data_capture(p_contact: &mut Contact) {
+    
+    let name = input_capture("Enter First Name");
+    let surname = input_capture("Enter Surname");
+    let dob = input_capture("Enter date of birth");
+    let address = input_capture("Enter your address");
+    let tel = input_capture("Enter your telephone number");
+    let email = input_capture("Enter email address");
+
+    p_contact.first_name = name;
+    p_contact.surname = surname;
+    p_contact.date_of_birth = dob;
+    p_contact.address = address;
+    p_contact.tel = tel;
+    p_contact.email = email;
+
+    p_contact.data_push()
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut contact = Contact{..Default::default()};
+
+    user_data_capture(&mut contact);
 }
